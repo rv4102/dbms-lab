ea9bc7ba833505c34d72c4fe4cfc0510dff4612d
Rushil Venkateswar <rushilv14@gmail.com>
2023-03-01 18:37:38 +0530

    change structure

--- A4/HMS/__init__.py
+++ A4/HMS/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 # from flask_bcrypt import Bcrypt
 # from flask_login import LoginManager
 from flask_session import Session
-from flask import render_template
+# from flask import render_template
 
 app = Flask(__name__)
 
@@ -20,30 +20,31 @@ Session(app)
 # bcrypt = Bcrypt(app)
 # login_manager = LoginManager(app)
 
+from HMS import routes
 
 # routing
 # 127.0.0.1/5000/   
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route('/patients')
-def patients():
-    return render_template('patients.html')
+# @app.route('/patients')
+# def patients():
+#     return render_template('patients.html')
 
-@app.route('/doctors')
-def doctors():
-    return render_template('doctors.html')
+# @app.route('/doctors')
+# def doctors():
+#     return render_template('doctors.html')
 
-@app.route('/appointments')
-def appointments():
-    return render_template('appointments.html')
+# @app.route('/appointments')
+# def appointments():
+#     return render_template('appointments.html')
 
-@app.route('/tests')
-def tests():
-    return render_template('tests.html')
+# @app.route('/tests')
+# def tests():
+#     return render_template('tests.html')
 
-@app.route('/admissions')
-def admissions():
-    return render_template('admissions.html')
+# @app.route('/admissions')
+# def admissions():
+#     return render_template('admissions.html')
 
--- A4/HMS/routes.py
+++ A4/HMS/routes.py
@@ -1,5 +1,32 @@
-# from HMS import app
-from flask import Blueprint
+from HMS import app
+from flask import render_template
+
+# routing
+# 127.0.0.1/5000/   
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/patients')
+def patients():
+    return render_template('patients.html')
+
+@app.route('/doctors')
+def doctors():
+    return render_template('doctors.html')
+
+@app.route('/appointments')
+def appointments():
+    return render_template('appointments.html')
+
+@app.route('/tests')
+def tests():
+    return render_template('tests.html')
+
+@app.route('/admissions')
+def admissions():
+    return render_template('admissions.html')
+
 
 
 
